c865c1942da21ebcc0a0db65e53b991bf8fc7275
Otavio <otaviosalomao@gmail.com>
2016-12-04 17:58:03 -0200

    Implementacao do controle de acesso

---
+++ auth.js
@@ -0,0 +1,3 @@
+
+var basicAuth = require('basic-auth');
+
--- server.js
+++ server.js
@@ -4,12 +4,27 @@ var appConfig = require('./config/appConfig');
 var cors = require('cors');
 var app = express();
 var router = express.Router();
-app.use(bodyParser());
-require('./controllers/productController')(router);
-require('./controllers/userController')(router);
-require('./controllers/loginController.js')(router);
-require('./controllers/accessController.js')(router);
 app.use(cors());
+app.use(bodyParser());
+require('./controllers/loginController')(router);
+router.use(function(req,res,next){	
+	if(req.headers.token && req.headers.user_id){
+		var Access = require("./models/access"); 			
+		Access.build().retrieveByTokenUserId(req.headers.token, req.headers.user_id, function(access){
+			if(access){				
+				require('./controllers/productController')(router);
+			 	require('./controllers/userController')(router);
+			}
+			else{
+				res.send(401, "Não Autorizado.");
+			}
+		});
+	}
+	else{
+		res.send(401, "Não Autorizado.");
+	}
+	
+});
 app.use('/api', router);
 app.listen(appConfig.port);
 console.log("Server was started.");
